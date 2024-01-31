d3a3abab57027d87bacb2914885cda7601cc86dd
Vasundhhara <vsk.isha16@gmail.com>
2024-01-31 23:11:43 +0530

    change the flow of APIs to take query as user input

--- app.py
+++ app.py
@@ -8,9 +8,17 @@ from test import auth_google
 st.title('Hushh-emails')
 
 st.write("Enter your query")
+gmail_query= st.text_input("Query")
 
 if st.button('Login to your google account'):
-    login = requests.get(url= "http://127.0.0.1:8000/login/google")
+    access_token = requests.get(url= "http://127.0.0.1:8000/login/google")
+    json_data = {
+    "access_token":access_token,
+    "user_query": gmail_query,
+}
+    response = requests.post("http://127.0.0.1:8000/auth/google", json=json_data)
+    print(response)
+    #query= requests.get(url="http://127.0.0.1:8000/auth/google")
     # token = login.text
     # fetch = requests.get(url= "http://127.0.0.1:8000/auth/google/?code={token}")
 
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.security import OAuth2PasswordBearer
 import requests
 #from jose import jwt
 import webbrowser
 import base64
 import json
+from pydantic import BaseModel
 from fastapi import Query
+
+class Item(BaseModel):
+    access_token:str
+    user_query:str
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -24,6 +29,7 @@ GOOGLE_REDIRECT_URI = web_credentials.get("redirect_uris", [])[0]  # Assuming th
 
 @app.get("/login/google")
 async def login_google():
+
     # oauth_url = f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline"
     
     #Below is the URL to prompt the user to login to his specified gmail account and also give a readonly access to his gmail
@@ -34,10 +40,8 @@ async def login_google():
         "url": f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline"
     }
 
-@app.get("/auth/google")
-async def auth_google(code: str):
-    #This code is basically the authorization code and this authorization code helps us to get the access token with the required scopes that we have set .
-    #We require the gmail.readonly scopes that requires verification of our application and all.
+@app.get("/get_access_token/google")
+async def get_access_token(code: str):
     token_url = "https://accounts.google.com/o/oauth2/token"
     print(code)
     data = {
@@ -50,11 +54,33 @@ async def auth_google(code: str):
     response = requests.post(token_url, data=data)
     access_token = response.json().get("access_token")
     print(response.json())
+
+    return {"access_token":access_token}
+
+@app.post("/auth/google")
+async def auth_google(request: Request):
+    data = await request.json()
+    access_token = data.get("access_token")
+    gmail_query = data.get("user_query")
+    #This code is basically the authorization code and this authorization code helps us to get the access token with the required scopes that we have set .
+    #We require the gmail.readonly scopes that requires verification of our application and all.
+    # token_url = "https://accounts.google.com/o/oauth2/token"
+    # print(code)
+    # data = {
+    #     "code": code,
+    #     "client_id": GOOGLE_CLIENT_ID,
+    #     "client_secret": GOOGLE_CLIENT_SECRET,
+    #     "redirect_uri": GOOGLE_REDIRECT_URI,
+    #     "grant_type": "authorization_code",
+    # }
+    # response = requests.post(token_url, data=data)
+    # access_token = response.json().get("access_token")
+    # print(response.json())
     user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
     #You can change this job query to get the specific documents 
     #jobs_query = "subject:new application:iOS Developer has:attachment after:2023/11/01 "
-    jobs_query = "subject:new application:iOS Developer has:attachment after:2023/11/01 before:2023/12/14"
-    #jobs_query= custom_parameter
+    #jobs_query = "subject:new application:iOS Developer has:attachment after:2023/11/01 before:2023/12/14"
+    jobs_query= gmail_query
     gmail_url = f"https://www.googleapis.com/gmail/v1/users/me/messages?q={jobs_query}&maxResults=1"
     gmail_response = requests.get(gmail_url, headers={"Authorization": f"Bearer {access_token}"})
     messages = gmail_response.json().get("messages", [])
